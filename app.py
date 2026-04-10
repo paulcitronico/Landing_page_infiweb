3248bb94ce5a28f61b1ad1d4fd6bf60e53d0b4df
simsimi2143 <josuerivasvergara@gmail.com>
2026-04-09 23:43:18 -0400

    git guardian cago todo

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 # os.environ.get busca la variable que configuraste en Railway
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') 
